859b939402e4cf78b3734025a815513cf3484ed1
Sydney Wong <sydneymwong@gmail.com>
2017-12-14 19:19:49 -0800

    added LDA

--- read_in_and_process.py
+++ read_in_and_process.py
@@ -1,5 +1,8 @@
 import os
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.decomposition import LatentDirichletAllocation
+
+num_topics = 5
 
 def get_topics():
 	directory = os.getcwd()
@@ -22,11 +25,26 @@ def read_in_and_save(topic):
 	return toReturn
 
 topics = get_topics()
-documents = {}
+documents = []
+topic_lengths = {}
 for topic in topics:
-	documents[topic] = read_in_and_save(topic)
+    docs_list = read_in_and_save(topic)
+    documents = documents + docs_list
+    topic_lengths[topic] = len(docs_list)
 
-vectorizer = TfidfVectorizer(min_df=1)
-X = vectorizer.fit_transform(documents.values())
-	
+vectorizer = CountVectorizer(min_df=1, stop_words = "english")
+X = vectorizer.fit_transform(documents)
+tf_feature_names = vectorizer.get_feature_names()
+
+lda = LatentDirichletAllocation(n_components=num_topics, max_iter=5, learning_method='online', learning_offset=50.,random_state=0).fit(X)
+
+def display_topics(model, feature_names, no_top_words):
+    for topic_idx, topic in enumerate(model.components_):
+        print("Topic %d:" % (topic_idx))
+        print(" ".join([feature_names[i]
+                        for i in topic.argsort()[:-no_top_words - 1:-1]]))
+    
+display_topics(lda, tf_feature_names, 10)
+
+
 	
