3284cfbe365d0c847457384aa90fa8bc6747f09c
Sydney Wong <sydneymwong@gmail.com>
2017-12-18 17:58:08 -0800

    still more stuff to fix but more filled out now

--- read_in_and_process.py
+++ read_in_and_process.py
@@ -1,6 +1,10 @@
 import os
-from sklearn.feature_extraction.text import CountVectorizer
+import csv
+from string import punctuation
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
+from collections import Counter
 
 num_topics = 5
 
@@ -13,16 +17,27 @@ def get_topics():
 			toReturn.append(filename)
 	return toReturn
 
+def clean_string(s):
+    toReturn = ''.join(c for c in s if c not in punctuation)
+    toReturn = ' '.join(toReturn.split())
+    return toReturn
+
 def read_in_and_save(topic):
-	directory = os.getcwd()
-	directory = os.path.join(directory, "bbc-fulltext", "bbc", topic)
-	toReturn = []
-	for filename in os.listdir(directory):
-		with open(os.path.join(directory, filename)) as file:
-			data = file.read()
-			toReturn.append(data)
-			#toReturn.append(data.split())
-	return toReturn
+    directory = os.getcwd()
+    directory = os.path.join(directory, "bbc-fulltext", "bbc", topic)
+    toReturn = []
+    counter = 0
+    for filename in os.listdir(directory):
+        with open(os.path.join(directory, filename)) as file:
+            if counter == 50:
+                break
+            else:
+                data = file.read()
+                data = clean_string(data)
+                toReturn.append(data)
+                #toReturn.append(data.split())
+            counter += 1
+    return toReturn
 
 topics = get_topics()
 documents = []
@@ -32,9 +47,82 @@ for topic in topics:
     documents = documents + docs_list
     topic_lengths[topic] = len(docs_list)
 
-vectorizer = CountVectorizer(min_df=1, stop_words = "english")
+#vectorizer = CountVectorizer(min_df=1, stop_words = "english")
+vectorizer = TfidfVectorizer(stop_words = "english", sublinear_tf=True)
 X = vectorizer.fit_transform(documents)
+idf = vectorizer.idf_
+idf = dict(zip(vectorizer.get_feature_names(), idf))
 tf_feature_names = vectorizer.get_feature_names()
+for key in idf.keys():
+    if key not in tf_feature_names:
+        print(key)
+        
+# store term frequencies in a list where each element represents a document 
+documents_tf = []
+for i in range(len(documents)):
+    documents_tf.append(Counter(documents[i].split()))
+    
+# store UNIVERSAL term frequencies in a list where each element represents a term 
+documents_split = []
+for i in range(len(documents)):
+    documents_split = documents_split + documents[i].split()
+universal_term_freq_dict = Counter(documents_split)
+universal_term_freq = []
+for term in tf_feature_names:
+    universal_term_freq.append(universal_term_freq_dict[term])
+    
+    
+# create a new list of dictionaries where each element represents a document 
+# {'word index' : tf*idf}
+
+documents_word_indices = []
+#documents_word_indices = pd.DataFrame()
+documents_tfidf_values = []
+#documents_tfidf_values = pd.DataFrame()
+for i in range(len(documents_tf)):
+    current_word_indices = ""
+    current_tfidf_values = ""
+    for key in documents_tf[i].keys():
+        if key in idf.keys():
+            #current[key] = documents_tf[i][key] * idf[key]
+            #current[tf_feature_names.index(key)] = documents_tf[i][key] * idf[key]
+            current_word_indices = " ".join((current_word_indices, str(tf_feature_names.index(key))))
+            #current_word_indices.append(tf_feature_names.index(key))
+            current_tfidf_values = " ".join((current_tfidf_values, str(round(documents_tf[i][key] * idf[key], 0))))
+            #current_tfidf_values.append(documents_tf[i][key] * idf[key])
+    documents_word_indices.append([current_word_indices])
+    documents_tfidf_values.append([current_tfidf_values])
+    #documents_word_indices[i] = current_word_indices
+    #documents_tfidf_values[i] = current_tfidf_values
+
+#data = vectorizer.fit_transform(tf_feature_names).toarray()
+
+"""
+# output the idf values to a csv
+with open('idfvalues.csv', 'w') as f:
+    w = csv.DictWriter(f, idf.keys())
+    w.writeheader()
+    w.writerow(idf)
+"""
+with open("word_indices.csv", "w", newline = '') as f:
+    writer = csv.writer(f)
+    writer.writerows(documents_word_indices)
+
+with open("tfidf_values.csv", "w", newline = '') as f:
+    writer = csv.writer(f)
+    writer.writerows(documents_tfidf_values)
+
+# output the feature names to a csv as "vocab"
+with open("vocab.csv", "w") as output:
+    writer = csv.writer(output, lineterminator='\n')
+    for val in tf_feature_names:
+        writer.writerow([val]) 
+        
+# output the universal term frequencies as "total_term_freq"
+with open("total_term_freq.csv", "w") as output:
+    writer = csv.writer(output, lineterminator='\n')
+    for val in universal_term_freq:
+        writer.writerow([val]) 
 
 lda = LatentDirichletAllocation(n_components=num_topics, max_iter=5, learning_method='online', learning_offset=50.,random_state=0).fit(X)
 
--- visualization.R
+++ visualization.R
@@ -1,42 +1,57 @@
 #install.packages("LDAvis")
 #install.packages("tm")
 #install.packages("lda")
+#install.packages("servr")
 
 library(LDAvis)
 library(tm)
 library(lda)
-stop_words <- stopwords("SMART")
+library(servr)
 
-# Get a list of topics which are the directory names inside the "bbc" folder
-get_topics <- function() {
-  directory <- getwd()
-  directory <- file.path(directory, "Github", "BBC_topic_modeling", "bbc-fulltext", "bbc")
-  toReturn = list.dirs(path = directory, full.names = FALSE, recursive = FALSE)
+# stop_words <- stopwords("SMART")
+# 
+# # Get a list of topics which are the directory names inside the "bbc" folder
+# get_topics <- function() {
+#   directory <- getwd()
+#   directory <- file.path(directory, "Github", "BBC_topic_modeling", "bbc-fulltext", "bbc")
+#   toReturn = list.dirs(path = directory, full.names = FALSE, recursive = FALSE)
+# }
+# 
+# topics <- get_topics()
+# 
+# # Get a list of the documents
+# read_in_and_save <- function(topic) {
+#   directory <- getwd()
+#   directory <- file.path(directory, "Github", "BBC_topic_modeling", "bbc-fulltext", "bbc", topic)
+#   for filename in list.files(path = directory) {
+#     read.delim(file.path(directory, filename), sep = "\n", header = FALSE)
+#   }
+# }
+
+directory <- getwd()
+#directory <- file.path(directory, "Github", "BBC_topic_modeling")
+index <- read.csv(file.path(directory, "word_indices.csv"), header = FALSE)
+value <- read.csv(file.path(directory, "tfidf_values.csv"), header = FALSE)
+vocab <- read.csv(file.path(directory, "vocab.csv"), header = FALSE)
+vocab <- as.character(vocab[,1])
+term.frequency <- read.csv(file.path(directory, "total_term_freq.csv"), header = FALSE)
+term.frequency <- as.integer(term.frequency[,1])
+
+# Put data in the form LDA wants
+documents <- list()
+for (i in seq(1, length(index))) {
+  #print(i)
+  current_index <- strsplit(as.character(index[i, 1][1]), " ")
+  current_index <- as.integer(current_index[[1]][-1])
+  current_value <- strsplit(as.character(value[i, 1][1]), " ")
+  current_value <- as.integer(current_value[[1]][-1])
+  mymatrix <- rbind(as.integer(current_index), as.integer(current_value))
+  documents[[i]] <- mymatrix
 }
-
-# Get a list of the documents
-read_in_and_save <- function(topic) {
-  directory <- getwd()
-  directory <- file.path(directory, "Github", "BBC_topic_modeling", "bbc-fulltext", "bbc", topic)
-  list.files(path = directory)
-}
-
-
-def read_in_and_save(topic):
-  directory = os.getcwd()
-directory = os.path.join(directory, "bbc-fulltext", "bbc", topic)
-toReturn = []
-for filename in os.listdir(directory):
-  with open(os.path.join(directory, filename)) as file:
-  data = file.read()
-toReturn.append(data)
-#toReturn.append(data.split())
-return toReturn
-
 
 # Fit LDA
 
-K <- 20
+K <- 5
 G <- 5000
 alpha <- 0.02
 eta <- 0.02
@@ -49,6 +64,7 @@ fit <- lda.collapsed.gibbs.sampler(documents = documents, K = K, vocab = vocab,
 
 theta <- t(apply(fit$document_sums + alpha, 2, function(x) x/sum(x)))
 phi <- t(apply(t(fit$topics) + eta, 2, function(x) x/sum(x)))
+doc.length <- sapply(documents, function(x) sum(x[2, ]))
 
 NewsArticles <- list(phi = phi,
                      theta = theta,
@@ -62,4 +78,4 @@ json <- createJSON(phi = NewsArticles$phi,
                    vocab = NewsArticles$vocab, 
                    term.frequency = NewsArticles$term.frequency)
 
-serVis(json, out.dir = 'vis', open.browser = FALSE)
+serVis(json, out.dir = file.path(directory, 'vis'), open.browser = FALSE)
